c0bee26eb4431b181d15fd311556052bddb1ae2a
Harrison Lynch <hblynch2001@gmail.com>
2022-03-18 14:35:34 -0700

    Visuals added VictoryBar/Chart into one file

--- client/components/Home.js
+++ client/components/Home.js
@@ -2,25 +2,26 @@ import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { VictoryBar } from 'victory'
 import { fetchAllPantries } from '../store/pantries'
+import Visuals from './Visuals'
 
 /**
  * COMPONENT
  */
 const Home = props => {
   const { username, id } = useSelector(state => state.auth)
-  
-  const { pantries } = useSelector(state => state.pantries) 
+
+  const { pantries } = useSelector(state => state.pantries)
 
   const dispatch = useDispatch()
 
-  useEffect(() => { 
+  useEffect(() => {
     dispatch(fetchAllPantries(id))
   }, []);
 
   return (
     <div>
       <h3>Welcome, {username}</h3>
-    <VictoryBar ></VictoryBar>
+      <Visuals />
     </div>
   )
 }
