c661ee17eb3b5a3d1127078e9eba73580b659a97
Evan Garris <egarris@gmail.com>
2022-03-18 17:02:26 -0500

    add: Visuals React component, data reducer

--- client/components/Visuals.js
+++ client/components/Visuals.js
@@ -1,38 +1,31 @@
-import React, { useEffect } from 'react'
-import { VictoryBar, VictoryChart } from 'victory'
-import { useSelector, useDispatch } from 'react-redux'
-import { fetchAllPantries } from '../store/pantries'
-import { captureRejections } from 'pg/lib/query'
+import React, { useEffect } from 'react';
+import { VictoryBar, VictoryChart } from 'victory';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchAllPantries } from '../store/pantries';
 
 const Visuals = () => {
-  const { username, id } = useSelector(state => state.auth)
-  const { pantries } = useSelector(state => state)
-  const dispatch = useDispatch()
+  const { username, id } = useSelector((state) => state.auth);
+  const { pantries } = useSelector((state) => state);
+  const dispatch = useDispatch();
 
-  const obj = {
-    { item: 'carrot', pantryQty: 3 }
-  }
-
-  const data = pantries.map(pantry => {
-    const ingredient = pantry.ingredients
-
-    // const { pantryQty }
-
-  })
-
-  console.log('ingredients: ', pantries)
+  const data = pantries.map((pantry) => {
+    return pantry.ingredients.map((ingredient) => {
+      return {
+        item: ingredient.name,
+        pantryQty: ingredient.pantryIngredient.pantryQty,
+      };
+    });
+  });
 
   useEffect(() => {
-    dispatch(fetchAllPantries(id))
+    dispatch(fetchAllPantries(id));
   }, []);
 
   return (
     <VictoryChart>
-    <VictoryBar
-
-      />
+      <VictoryBar data={data} />
     </VictoryChart>
-  )
-  }
+  );
+};
 
 export default Visuals;
