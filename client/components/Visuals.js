8540bbd95fb73f634c8d48a8918737e3de75bcf2
David Dunham <dd567@cornell.edu>
2022-03-21 11:48:32 -0400

    added pantry dropdown selector to the homepage

--- client/components/Visuals.js
+++ client/components/Visuals.js
@@ -8,23 +8,38 @@ const Visuals = () => {
   const { pantries } = useSelector((state) => state);
   const dispatch = useDispatch();
 
-  const data = pantries.map((pantry) => {
-    return pantry.ingredients.map((ingredient) => {
-      return {
-        item: ingredient.name,
-        pantryQty: ingredient.pantryIngredient.pantryQty,
-      };
-    });
-  });
+  const data = pantries
+    .map((pantry) => {
+      return pantry.ingredients.map((ingredient) => {
+        return {
+          item: ingredient.name,
+          pantryQty: Number(ingredient.pantryIngredient.pantryQty),
+        };
+      });
+    })
+    .reduce((prev, curr) => {
+      return prev.concat(curr);
+    }, []);
 
   useEffect(() => {
     dispatch(fetchAllPantries(id));
   }, []);
 
   return (
-    <VictoryChart>
-      <VictoryBar data={data} />
-    </VictoryChart>
+    <div>
+      <label htmlFor="pantries">Choose a pantry: </label>
+      <select name="pantries">
+        <option value="">View All Pantries</option>
+        {pantries.map((pantry) => (
+          <option key={pantry.id} value={pantry.name}>
+            {pantry.name}
+          </option>
+        ))}
+      </select>
+      <VictoryChart domainPadding={{ x: 15 }}>
+        <VictoryBar data={data} x="item" y="pantryQty" />
+      </VictoryChart>
+    </div>
   );
 };
 
