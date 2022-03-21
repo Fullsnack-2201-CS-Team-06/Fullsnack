67e08e6912aa6826f5f96afcfc1a50fcc0459bd7
Evan Garris <egarris@gmail.com>
2022-03-21 12:00:21 -0500

    add: chart selector & data formatting

--- client/components/Visuals.js
+++ client/components/Visuals.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { VictoryBar, VictoryChart } from 'victory';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchAllPantries } from '../store/pantries';
@@ -6,25 +6,66 @@ import { fetchAllPantries } from '../store/pantries';
 const Visuals = () => {
   const { username, id } = useSelector((state) => state.auth);
   const { pantries } = useSelector((state) => state);
-  const dispatch = useDispatch();
 
-  const data = pantries.map((pantry) => {
-    return pantry.ingredients.map((ingredient) => {
-      return {
-        item: ingredient.name,
-        pantryQty: ingredient.pantryIngredient.pantryQty,
-      };
-    });
-  });
+  const [selectedPantry, setSelectedPantry] = useState('View All Pantries');
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchAllPantries(id));
   }, []);
 
+  let data = pantries.map((pantry) => {
+    const contents = pantry.ingredients.map((ingredient) => {
+      return {
+        item: ingredient.name,
+        pantryQty: ingredient.pantryIngredient.pantryQty,
+      };
+    });
+
+    return {
+      name: pantry.name,
+      contents: contents,
+    };
+  });
+
+  const selectPantry = (pantries, pantryName) => {
+    const selectedPantry = pantries.filter(
+      (pantry) => pantry.name === pantryName
+    );
+    return selectedPantry[0].contents;
+  };
+
+  const reducePantries = (pantries) => {
+    return pantries.reduce((prev, curr) => {
+      return prev.concat(curr.contents);
+    }, []);
+  };
+
+  if (selectedPantry === 'View All Pantries') {
+    data = reducePantries(data);
+  } else {
+    data = selectPantry(data, selectedPantry);
+  }
+
+  const handlePantryChange = (e) => {
+    setSelectedPantry(e.target.value);
+  };
+
   return (
-    <VictoryChart>
-      <VictoryBar data={data} />
-    </VictoryChart>
+    <div>
+      <select name="pantries" onChange={(e) => handlePantryChange(e)}>
+        <option value="View All Pantries">View All Pantries</option>
+        {pantries.map((pantry) => (
+          <option key={pantry.id} value={pantry.name}>
+            {pantry.name}
+          </option>
+        ))}
+      </select>
+      <VictoryChart>
+        <VictoryBar data={data} x="item" y="pantryQty" />
+      </VictoryChart>
+    </div>
   );
 };
 
