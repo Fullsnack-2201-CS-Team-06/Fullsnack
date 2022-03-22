871b99cb72a92b714fde5955c237d9d9090cf862
Kevin Veloso <klveloso24@gmail.com>
2022-03-22 11:14:17 -0500

    Pantry names render in Pantries tab

--- client/components/Pantries.js
+++ client/components/Pantries.js
@@ -3,31 +3,29 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchAllPantries } from '../store/pantries';
 
-
-
 const Pantries = () => {
-
-  const { username, id } = useSelector(state => state.auth)
-  const { pantries } = useSelector(state => state)
-  //Ask about state.pantries versus state
+  const { username, id } = useSelector((state) => state.auth);
+  const { pantries } = useSelector((state) => state);
 
   const dispatch = useDispatch();
-  console.log("What's on my state", pantries, pantries.length)
 
   useEffect(() => {
-    dispatch(fetchAllPantries(id))
-  }, [])
+    dispatch(fetchAllPantries(id));
+  }, []);
 
-
-  return (<div>
-  <h3>My Pantry</h3>
-  {pantries.length ? (pantries.map((pantry) => {
-  <Link key={pantry.id} to ="/">{pantry.name}</Link>
-  }))
-: (
-  <div>Nothing to show</div>
-)
-}</div>);
+  return (
+    <div>
+      <h3>My Pantry</h3>
+      {pantries.length > 0 ? (
+        pantries.map((pantry) => {
+         return <li key={pantry.id}><Link to="/">{pantry.name}</Link></li>
+        ;
+        })
+      ) : (
+        <div>Nothing to show</div>
+      )}
+    </div>
+  );
 };
 
 export default Pantries;
