d1d50ba3a39237070bffdd8d232154f6bc05c768
Kevin Veloso <klveloso24@gmail.com>
2022-03-22 16:08:07 -0500

    Everything is wired correctly. Ingredients are on SinglePantry state, but have to be rendered.

--- client/components/PantrySingle.js
+++ client/components/PantrySingle.js
@@ -1,18 +1,28 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams } from 'react-router';
 import Container from 'react-bootstrap/Container';
 import Table from 'react-bootstrap/Table';
 import { fetchOnePantry } from '../store/pantry';
 
-const PantrySingle = ( { match }) => {
+const PantrySingle = ({ match }) => {
   const dispatch = useDispatch();
   const { id } = useSelector((state) => state.auth);
-  // const { pantry } = useSelector((state) => state);
+  const { pantry } = useSelector((state) => state);
+
+  console.log("What's on my state", pantry.ingredients);
+  // console.log("What's in my map", data)
 
   useEffect(() => {
     dispatch(fetchOnePantry(match.params.id));
   }, []);
+
+  /**
+   *  Note to self, I have to grab item quantity from
+   *  pantry ingredients.
+   * 
+   * 
+   */
+
 
   return (
     <div className='PantrySingle'>
@@ -28,14 +38,18 @@ const PantrySingle = ( { match }) => {
             </tr>
           </thead>
           <tbody>
-            {/* {singlePantry.map((ingredient) => {
-              <tr>
-                <td>{ingredient.name}</td>
-                <td>{Placeholder}</td>
-                <td>{ingredient.cost}</td>
-                <td>{ingredient.uom}</td>
-              </tr>;
-            })} */}
+            {pantry > 0 ? (
+              pantry.ingredients.map((ingredient) => { return
+                <tr>
+                  <td>{ingredient.name}</td>
+                  <td>{Placeholder}</td>
+                  <td>{ingredient.cost}</td>
+                  <td>{ingredient.uom}</td>
+                </tr>;
+              })
+            ) : (
+              <td>Nothing to see here.</td>
+            )}
           </tbody>
         </Table>
       </Container>
