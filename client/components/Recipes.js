f85efd4e12f01e0dfac59cb6cc418a492b3da1f7
Evan Garris <egarris@gmail.com>
2022-03-22 15:40:16 -0500

    add: all user recipes rendering (no styling)

--- client/components/Recipes.js
+++ client/components/Recipes.js
@@ -18,7 +18,34 @@ const Recipes = () => {
 
   return (
     <div>
-      <h1>My Recipes</h1>
+      <div>
+        <h1>My Recipes</h1>
+      </div>
+      <div>
+        {recipes.map((recipe) => {
+          return (
+            <div key={recipe.id}>
+              <h3>{recipe.name}</h3>
+              <img src={recipe.image} style={{ height: '200px' }} />
+              <br />
+              <br />
+              {recipe.ingredients.map((ingredient) => {
+                return (
+                  <div key={ingredient.id}>
+                    <span>{ingredient.name} </span>
+                    <span>{ingredient.recipeIngredient.recipeQty}</span>
+                    <span>{ingredient.uom}</span>
+                  </div>
+                );
+              })}
+              <p>{recipe.description}</p>
+              <h5>{recipe.rating} of 5 stars</h5>
+            </div>
+          );
+        })}
+      </div>
+      <br />
+      <br />
     </div>
   );
 };
