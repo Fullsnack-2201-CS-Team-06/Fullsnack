41d30764354b4a9d0d541a1a1bd6f9c9b1389264
David Dunham <dd567@cornell.edu>
2022-03-22 15:07:05 -0400

    allfoods searches for ingredients on the pantries, but also the recipes and shopping lists

--- server/api/ingredients.js
+++ server/api/ingredients.js
@@ -2,11 +2,12 @@ const router = require('express').Router();
 module.exports = router;
 const Ingredient = require('../db/models/Ingredient');
 const Pantry = require('../db/models/Pantry');
+const ShoppingList = require('../db/models/ShoppingList');
+const Recipe = require('../db/models/Recipe');
 
 //GET /api/ingredients?userId=INT
 router.get('/', async (req, res, next) => {
   try {
-    //INCOMPLETE. How do we get ingredient info associated only with one user? Cost depends on a user's input (Users buy at different locations). We need a direct association between the user and their ingredients with the cost on a through table. For now, I will render all ingredients that are on a user's pantries. Changing the cost will do so for all users.
     let ingredients = [];
 
     //Get all the pantries of the user and their associated ingredients.
@@ -15,22 +16,53 @@ router.get('/', async (req, res, next) => {
       include: Ingredient,
     });
 
-    if (pantries) {
-      //Get all the ingredients associated with all the pantries.
+    //All the user's shopping lists.
+    const shoppingLists = await ShoppingList.findAll({
+      where: { userId: req.query.userId },
+      include: Ingredient,
+    });
+
+    //All the user's recipes.
+    const recipes = await Recipe.findAll({
+      where: { userId: req.query.userId },
+      include: Ingredient,
+    });
+
+    if (pantries.length > 0) {
+      //Get all the ingredients associated with all the user's pantries.
       ingredients = pantries.reduce((prev, pantry) => {
         return prev.concat(pantry.ingredients);
       }, []);
-
-      //Eliminate duplicates of ingredients existing in multiple pantries.
-      const foundFoods = [];
-      ingredients = ingredients.filter((food) => {
-        const isFound = foundFoods.includes(food.id);
-        if (!isFound) {
-          foundFoods.push(food.id);
-        }
-        return !isFound;
-      });
     }
+
+    if (shoppingLists.length > 0) {
+      //Get all the ingredients associated with the user's shopping lists.
+      ingredients.concat(
+        shoppingLists.reduce((prev, list) => {
+          return prev.concat(list.ingredients);
+        })
+      );
+    }
+
+    if (recipes.length > 0) {
+      //Get all the ingredients associated with the user's recipes.
+      ingredients.concat(
+        recipes.reduce((prev, recipe) => {
+          return prev.concat(recipe.ingredients);
+        })
+      );
+    }
+
+    //Eliminate duplicates of ingredients existing in multiple pantries.
+    const foundFoods = [];
+    ingredients = ingredients.filter((food) => {
+      const isFound = foundFoods.includes(food.id);
+      if (!isFound) {
+        foundFoods.push(food.id);
+      }
+      return !isFound;
+    });
+
     res.send(ingredients);
   } catch (error) {
     next(error);
