4fcbc82766c4ae3326fc36b29ec9f4d0ecf1181e
Evan Garris <egarris@gmail.com>
2022-03-23 11:51:26 -0500

    add: more info to single recipe component

--- client/components/SingleRecipe.js
+++ client/components/SingleRecipe.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import { fetchSingleRecipe } from '../store/singleRecipe';
 
 const SingleRecipe = () => {
@@ -11,9 +11,10 @@ const SingleRecipe = () => {
   });
 
   const dispatch = useDispatch();
+  const history = useHistory();
   const { id } = useParams();
 
-  const ingredients = singleRecipe.ingredients || []
+  const ingredients = singleRecipe.ingredients || [];
 
   useEffect(() => {
     dispatch(fetchSingleRecipe(id));
@@ -22,17 +23,30 @@ const SingleRecipe = () => {
   return (
     <div>
       <h1>{singleRecipe.name}</h1>
+      <h5>{singleRecipe.rating} of 5 stars</h5>
       <img src={singleRecipe.image} style={{ height: '400px' }} />
       <br />
-      {ingredients.map((ingredient) => {
-        return (
-          <div key={ingredient.id}>
-            <span>{ingredient.name} </span>
-            <span>{ingredient.recipeIngredient.recipeQty}</span>
-            <span>{ingredient.uom}</span>
-          </div>
-        );
-      })}
+      <br />
+      <ul>
+        {ingredients.map((ingredient) => {
+          return (
+            <li key={ingredient.id}>
+              <span>{ingredient.name} </span>
+              <span>{ingredient.recipeIngredient.recipeQty}</span>
+              <span>{ingredient.uom}</span>
+            </li>
+          );
+        })}
+      </ul>
+      <h5>Description</h5>
+      <p>{singleRecipe.description}</p>
+      <h5>Nutritional Info</h5>
+      <h6>
+        <span>Carbs: {singleRecipe.carbsPerRecipe} </span>
+        <span>Fat: {singleRecipe.fatPerRecipe} </span>
+        <span>Protein: {singleRecipe.proteinPerRecipe} </span>
+      </h6>
+      <button onClick={() => history.push('/recipes')}>Back</button>
     </div>
   );
 };
