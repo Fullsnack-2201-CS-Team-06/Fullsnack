66d4210e978d31d364dace3266a02db2581b8a7c
David Dunham <dd567@cornell.edu>
2022-03-29 17:13:29 -0400

    aborted attempt to get food api data and reverted changes

--- script/seed.js
+++ script/seed.js
@@ -18,6 +18,7 @@ const {
  * seed - this function clears the database, updates tables to
  *      match the models, and populates the database.
  */
+
 async function seed() {
   await db.sync({ force: true }); // clears db and matches models to tables
   console.log('db synced!');
