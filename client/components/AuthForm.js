a014cede55b602ca05aae42bb66eea3c6aa2e2be
Evan Garris <egarris@gmail.com>
2022-03-29 15:57:32 -0500

    add: more styling to login auth form

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -20,31 +20,37 @@ const AuthForm = ({ name, displayName }) => {
   };
 
   return (
-    <div className={styles.authFormContainer}>
-      <Form className={styles.authForm} onSubmit={handleSubmit} name={name}>
-        <div>
-          <Form.Label htmlFor="username">Username</Form.Label>
-          <Form.Control
-            name="username"
-            type="text"
-            placeholder="Enter username"
-          />
-        </div>
-        <div>
-          <Form.Label htmlFor="password">Password</Form.Label>
-          <Form.Control
-            name="password"
-            type="password"
-            placeholder="Enter Password"
-          />
-        </div>
-        <div>
-          <Button variant="primary" className={styles.button} type="submit">
-            {displayName}
-          </Button>
-        </div>
-        {error && error.response && <div> {error.response.data} </div>}
-      </Form>
+    <div className={styles.authPageContainer}>
+      <div className={styles.authFormContainer}>
+        <Form className={styles.authForm} onSubmit={handleSubmit} name={name}>
+          <h1 className={styles.formTitle}>Login</h1>
+          <div>
+            <Form.Label htmlFor="username">Username</Form.Label>
+            <Form.Control
+              name="username"
+              type="text"
+              placeholder="Enter username"
+            />
+          </div>
+          <div>
+            <Form.Label htmlFor="password">Password</Form.Label>
+            <Form.Control
+              name="password"
+              type="password"
+              placeholder="Enter Password"
+            />
+          </div>
+          <div>
+            <Button variant="primary" className={styles.button} type="submit">
+              {displayName}
+            </Button>
+          </div>
+          {error && error.response && <div> {error.response.data} </div>}
+        </Form>
+      </div>
+      <div className={styles.authPageImg}>
+        <img src="https://imagesvc.meredithcorp.io/v3/mm/image?q=60&c=sc&poi=face&w=2000&h=1000&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F19%2F2007%2F09%2F17%2FMR_Reshoots_102219_016-2000.jpg" />
+      </div>
     </div>
   );
 };
