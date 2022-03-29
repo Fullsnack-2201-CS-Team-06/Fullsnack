510888eae8db845dada5c5e971a57c939a4b9069
David Dunham <dd567@cornell.edu>
2022-03-29 15:54:42 -0400

    account page has an image

--- client/components/Account.js
+++ client/components/Account.js
@@ -26,18 +26,30 @@ const Account = () => {
   }, [auth]);
 
   return showForm ? (
-    <div className={styles.container}>
-      <AccountUpdate />
-      <Button variant="secondary" onClick={toggleStatus}>
-        Cancel
-      </Button>
+    <div className={styles.page}>
+      <div className={styles.container}>
+        <AccountUpdate />
+        <Button variant="secondary" onClick={toggleStatus}>
+          Cancel
+        </Button>
+      </div>
+      <img
+        className={styles.image}
+        src="https://media.istockphoto.com/photos/fresh-kale-picture-id899247492?b=1&k=20&m=899247492&s=170667a&w=0&h=1C-yhVk9dP1-J0DZeWAmrW1WQhles4tpq7rUx4EQfO4="
+      />
     </div>
   ) : (
-    <div className={styles.container}>
-      <AccountDisplay />
-      <Button variant="primary" onClick={toggleStatus}>
-        Update
-      </Button>
+    <div className={styles.page}>
+      <div className={styles.container}>
+        <AccountDisplay />
+        <Button variant="primary" onClick={toggleStatus}>
+          Update
+        </Button>
+      </div>
+      <img
+        className={styles.image}
+        src="https://media.istockphoto.com/photos/fresh-kale-picture-id899247492?b=1&k=20&m=899247492&s=170667a&w=0&h=1C-yhVk9dP1-J0DZeWAmrW1WQhles4tpq7rUx4EQfO4="
+      />
     </div>
   );
 };
