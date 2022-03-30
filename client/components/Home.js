be431ca8420e2b7740a0de8c18e6eead25dded98
David Dunham <dd567@cornell.edu>
2022-03-30 15:06:47 -0400

    Draft box plot of food:calories on home page

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,31 +1,33 @@
-import React, { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { VictoryBar } from 'victory'
-import { fetchAllPantries } from '../store/pantries'
-import Visuals from './Visuals'
-import Visual2 from './Visual2'
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { VictoryBar } from 'victory';
+import { fetchAllPantries } from '../store/pantries';
+import Visuals from './Visuals';
+import Visual2 from './Visual2';
+import VisualNutrition from './VisualNutrition';
 
 /**
  * COMPONENT
  */
-const Home = props => {
-  const { username, id } = useSelector(state => state.auth)
+const Home = (props) => {
+  const { username, id } = useSelector((state) => state.auth);
 
-  const { pantries } = useSelector(state => state.pantries)
+  const { pantries } = useSelector((state) => state.pantries);
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchAllPantries(id))
+    dispatch(fetchAllPantries(id));
   }, []);
 
   return (
     <div>
       <h3>Welcome, {username}</h3>
+      <VisualNutrition />
       <Visuals />
       <Visual2 />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
