44813748a5a6b13359656e77b327463f4f467279
Harrison Lynch <hblynch2001@gmail.com>
2022-04-05 13:22:18 -0700

    removed div containter

--- client/components/VisualRecipeNutrition.js
+++ client/components/VisualRecipeNutrition.js
@@ -5,7 +5,9 @@ import {
   VictoryAxis,
   VictoryTheme,
   VictoryLabel,
-  VictoryGroup
+  VictoryGroup,
+  VictoryStack,
+  VictoryVoronoiContainer
 } from 'victory';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchAllRecipes } from '../store/recipes';
@@ -14,52 +16,74 @@ const VisualRecipeNutrition = () => {
   const { id } = useSelector(state => state.auth)
   const { recipes } = useSelector(state => state)
   const dispatch = useDispatch();
-  let protein = []
-  let carbs = []
-  let fats = []
-  let calorie = []
+  let dataset = []
 
   useEffect(() => {
     dispatch(fetchAllRecipes(id))
   }, [])
 
   if (recipes) {
-    recipes.forEach((singleRecipe, index) => {
-      calorie.push({ x: index, y: singleRecipe.caloriesPerRecipe })
-      carbs.push({ x: index, y: singleRecipe.carbsPerRecipe })
-      fats.push({ x: index, y: singleRecipe.fatPerRecipe })
-      protein.push({ x: index, y: singleRecipe.proteinPerRecipe })
+   dataset = recipes.map((singleRecipe) => {
+     console.log(typeof singleRecipe.caloriesPerRecipe)
+     console.log(typeof singleRecipe.carbsPerRecipe)
+      return [
+      { x: 'calories', y: (singleRecipe.caloriesPerRecipe).toString() },
+      { x: 'carbs', y: singleRecipe.carbsPerRecipe },
+      { x: 'fats', y: singleRecipe.fatPerRecipe },
+      { x: 'protein', y: singleRecipe.proteinPerRecipe },
+      ]
     })
   }
 
-  console.log('protein', protein)
+  console.log('dataset', dataset)
 
   return (
-    <div style={{ height: '650px' }}>
+    // <div style={{ height: '650px' }}>
+    <>
       <VictoryChart
-          theme={VictoryTheme.material}
-          domain={{ y: [0.5, 10.5] }}
+          domain={{ x: [0.5, 4.5], y: [0, 10] }}
+          // width={'800'}
+          // containerComponent={
+          // <VictoryVoronoiContainer
+          //   style={{}}
+          //   labels={({})}
+          // />
+        // }
         >
-          <VictoryGroup horizontal
-            offset={10}
-            style={{ data: { width: 6 } }}
-            colorScale={["brown", "tomato", "gold", "red"]}
+        <VictoryLabel
+          x={230}
+          y={25}
+          textAnchor="middle"
+          text="Nutrition of My Recipe's"
+          />
+        <VictoryStack
+          horizontal
+          colorScale={[
+            "#003f5c",
+            "#2f4b7c",
+            "#665191",
+            "#a05195",
+            "#d45087",
+            "#f95d6a",
+            "#ff7c43",
+            "#ffa600"
+          ]}
+          style={{
+            labels: { fontSize: 10 },
+            data: { stroke: "black", strokeWidth: 1 },
+            tickLabels: { fill: "none" },
+          }}
           >
-            <VictoryBar
-              data={calorie}
-            />
-            <VictoryBar
-              data={protein}
-            />
-            <VictoryBar
-              data={fats}
-            />
-            <VictoryBar
-              data={carbs}
-            />
-          </VictoryGroup>
-        </VictoryChart>
-    </div>
+        {dataset.map((data, i) => {
+          return <VictoryBar
+          data={data}
+          key={i}
+          binSpacing={2}
+             />;
+          })}
+        </VictoryStack>
+      </VictoryChart>
+      </>
   )
 }
 
